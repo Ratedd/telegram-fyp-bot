835fa52a00ead36233d3ce607a14bab9d63c772c
Royston Ang <32552526+Ratedd@users.noreply.github.com>
2019-04-18 05:34:40 +0800

    Added Winston package

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start(ctx => ctx.reply('Hi there!'));
+bot.start(ctx => ctx.reply('Hey there!'));
 
 bot.launch().then(() => {
 	// TODO IMPLEMENT LOGGER
