61adabc1113493f8905a5c79a3c3cf411e08a5ce
Royston Ang <32552526+Ratedd@users.noreply.github.com>
2019-07-23 05:59:36 +0800

    chore: minor changes

--- commands/subscribe.js
+++ commands/subscribe.js
@@ -1,18 +1,16 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = {
 	name: 'subscribe',
 	description: 'Subscribe to notification',
 	args: false,
 	async execute(bot, ctx) { // eslint-disable-line no-unused-vars
-		const subscriberInfo = {
+		const response = await axios.post(`${process.env.API_SERVER}/api/addsubscriber`, {
 			id: ctx.from.id,
 			name: ctx.from.first_name
-		};
-		const data = await fetch(`${process.env.API_SERVER}/api/addsubscriber`, { method: 'POST', body: JSON.stringify(subscriberInfo) })
-			.then(res => res.json());
+		});
 
-		if (!data.message) return ctx.reply('You have successfully subscribed to announcements');
-		return ctx.reply(data.message);
+		if (!response.data.message) return ctx.reply('You have successfully subscribed to announcements');
+		return ctx.reply(response.data.message);
 	}
 };
--- commands/unsubscribe.js
+++ commands/unsubscribe.js
@@ -1,17 +1,13 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = {
 	name: 'unsubscribe',
 	description: 'Subscribe to notification',
 	args: false,
 	async execute(bot, ctx) { // eslint-disable-line no-unused-vars
-		const subscriberInfo = {
-			id: ctx.from.id
-		};
-		const data = await fetch(`${process.env.API_SERVER}/api/removesubscriber`, { method: 'DELETE', body: JSON.stringify(subscriberInfo) })
-			.then(res => res.json());
+		const response = await axios.delete(`${process.env.API_SERVER}/api/removesubscriber/${ctx.from.id}`);
 
-		if (!data.message) return ctx.reply('You have successfully unsubscribed to announcements');
-		return ctx.reply(data.message);
+		if (!response.data.message) return ctx.reply('You have successfully unsubscribed to announcements');
+		return ctx.reply(response.data.message);
 	}
 };
